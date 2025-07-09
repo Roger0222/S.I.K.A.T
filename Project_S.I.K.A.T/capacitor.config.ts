3bb9b1aeab897e4b0f29305cfa0f2728296ae36e
Roger0222 <20211113@nbsc.edu.ph>
2025-07-09 21:39:50 +0800

    minor changes

--- Project_S.I.K.A.T/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'Project_S.I.K.A.T',
-  webDir: 'dist'
-};
-
-export default config;

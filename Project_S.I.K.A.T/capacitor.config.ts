d0a77607fec13b2f01a5029dfede81f9fc020240
jaamee-s <anticamarajame@gmail.com>
2025-07-09 21:07:20 +0800

    initial commits

---
+++ Project_S.I.K.A.T/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Project_S.I.K.A.T',
+  webDir: 'dist'
+};
+
+export default config;
